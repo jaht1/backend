b1ba601adc36a020cac3f1d9f424934c3d1d35b4
jaht1 <ahtijenn@arcada.fi>
2021-01-14 13:35:18 +0200

    commit nya startfilerna

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# backend
-Skriv om backend projektet
-### underrubrik
-även här går det att skriva
-
-### Nyaste ändringen i VS code
-Får se om Src code funkar
-
-### Hej dehär e test
-Funkar de?!?!
-
-
-Här är ändringen inför nästa lektion
+# back-end
+Vårt backendprojekt finns att klona härifrån
+### För att skapa en underrrubrik
+Även här kan man skriva om sitt backendprojekt
+### En förbättring trots att SKURK varit här
+Hejsvejs
+### En underrubrik till
+En otrolig förbättring till vårt backendprojekt
+### Nyaste ändringen i vscode
+Få se om source control funkar!
+### Jag skrev in user.name och user.email
+Hoppas commit funkar nu!
+### ENS SKURK HAR ÄNDRAT MEDAN DENNIS SATT I VSCODE
--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-Hejsan
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dennis JS Demo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- Containern har max bredd 800px -->
+    <div id="container">
+        <nav>
+            <!-- Logo och meny finns i nav -->
+            <ul>
+                <a id="current" href="home/">Home</a>
+                <a href="projekt1/">Projekt 1</a>
+                <a href="projekt2/">Projekt 2</a>
+            </ul>
+        </nav>
+
+        <!-- Artiklar placerar sig snyggt efter varann -->
+        <article>
+            <h1>Uppg 1</h1>
+            <p>Jag ändrar samma paragraf som thesourmango</p>
+        </article>
+
+        <?php
+        print(3+6);
+        // Uppg 1 - Superglobals
+        // phpinfo(); // Sök här efter uppg 1 info
+        print($_SERVER['REMOTE_USER']);
+        $serverPort = $_SERVER['SERVER_PORT'];
+        // Konkatenering med punkt, märk att PHP kod producerar HTML resurser
+        print("<p>Servern snurrar på port :" . $serverPort . "</p>" );
+        ?>
+
+        <article>
+            <h1>Uppg 2</h1>
+            <p>Thesourmango kodar vidare</p>
+            <p>och kommer strax att glömma pull inna push!</p>
+        </article>
+    </div>
+</body>
+
+<!-- Script kan köras efter att sidan laddats in -->
+<script src="script.js"></script>
+
+</html>
